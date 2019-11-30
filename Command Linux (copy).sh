21e07272da59cd88fe2683a78daa34e8720ee4b6
Oleg-Derkach <zzzorgjb@gmail.com>
2019-11-30 21:29:59 +0200

    Second Commit

---
+++ Command Linux (copy).sh
@@ -0,0 +1,104 @@
+Command for Linux system
+
+./file.sh  - file execution
+
+ls - 		list of dirrectory
+head [-5] [name of file]- read 5 strings of file from head
+	head -5 posit.sh
+tail [5] [name of file] - read 5 strings of file from down
+	tail -5 posit.sh
+tail -f posit.sh - allow user to read file in online mode	
+grep ['fragment'][filename] - serch occurrences of fragment inside the file
+	grep 'echo' posit.sh
+	grep aditional keys -c - quantity of occurrences
+	grep		        -n - full string of occurrences
+history  - shows the commands inserted into the bash
+	history | grep head  - returns the history inputs for bash with command head
+	For Example we need to find command which we insert into he bash, but the 
+	time life of the terminal is too long and we need to spend a lot of time
+	to find the command we need. In this case we use conveyor with command grep
+	How it works - command history return string type information with data,
+	then we follow this information to grep and as result command return for us
+	lines with occurrences 'head' in history   
+
+===================
+conveyor (|)
+history | grep ls -n
+history | grep 'ls' -n    - it is possible to use '' for atributes 
+'|' this symbol means that output result from 'history' follow to input to 
+command 'grep'
+==============	
+
+cal - 		calendar with current date
+date - 	current date and time
+	date +%A   current weekday
+	date +%B   current month
+
+wget [direct url path to file] downloader for linux. Allow user to download file
+	wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb	
+
+man [function] - manual(instraction) for function
+pwd      - my current location
+chmod +x(executable file)   change attribute for file
+	chmod +x hello-world.sh
+		#!/bin/bash
+		echo "Hello World"
+	"Hello World"
+	
+rm -r(recursively)   remove file or dirrectory recursively	
+
+Compress (not archive) files / folders
+tar -cvf(compress verbose file) [filename(result)] [filename(to comress)]
+tar -tf(test file) [filename]
+tar -xvf(extract verbose file) [filename]
+
+BASH_VERSION  
+
+======================================================
+VARIABLES
+
+user=$(whoami)            => whoami (returns the usersID)
+input=/home/$user
+If after variable need to insert the other characters, this variable necessary to
+cover with curly braces
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+======================================================
+Input, Output and Error Redirections
+
+Some ways to redirecting the output error massages
+
+ls -lR > dir-tree.list
+Redirect all list of the dir tree  to file dir-tree.list
+
+If we need to redirect error messages to text file
+ls -lR 2> dir-tree.list
+
+if we need to append messages to text file
+ls -lR 2>> dir-tree.list
+
+if we need to redirect any messages to text file
+ls -lR 2>&1 dir-tree.list
+
+==============================================================================
+Find method
+
+find $1 -type f   method  finds items type file    
+find $1 -type d   method  finds items type dirrectory
+
+===================================================
+if else Statemants
+
+if [ $num_a -lt $num_b ]; then
+    echo "$num_a is less than $num_b!"
+fi
+
+===================================================
+Positional argument parameters
+
+
+
+
+
+
+
+
